6dc194c4702250f1ae2b997a9fc7cc61e5264a27
Tuure Savuoja <tuure@tuuresavuoja.net>
2026-02-15 13:09:17 +0200

    Improve error handling in CLI

--- src/cli.ts
+++ src/cli.ts
@@ -7,6 +7,9 @@ const args = process.argv.slice(2)
 const allowUnsafeHtml = args.includes('--allow-unsafe-html')
 
 readFile(0, 'utf8', (err, stdin) => {
-  if (err) throw err
+  if (err) {
+    console.error(err.message)
+    process.exit(1)
+  }
   process.stdout.write(convertToHtml(stdin, { allowUnsafeHtml }))
 })
